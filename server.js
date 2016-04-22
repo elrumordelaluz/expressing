3e6ba1494f9afb34d508d9caf7f4f4e5a6210ce1
Lionel T <elrumordelaluz@hotmail.com>
2016-04-22 10:27:02 +0200

    Parse request URL

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var http = require("http");
 
 function requestHandler(request, response) {
-  console.log("In comes a request to: " + request.url);
-  response.end("Hello, world!");
+  if (request.url === "/") {
+    response.end("Welcome to the homepage!");
+  } else if (request.url === "/about") {
+    response.end("Welcome to the about page!");
+  } else {
+    response.end("Error! File not found.");
+  }
 }
 
 var server = http.createServer(requestHandler);
