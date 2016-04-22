b2e0d3219473b38d5df2731b342507dece47fcba
Lionel T <elrumordelaluz@hotmail.com>
2016-04-22 11:46:56 +0200

    Use express.static

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 var express = require('express');
-var logger = require('morgan');
+var path = require('path');
 var http = require('http');
 
 var app = express();
 
-app.use(logger('short'));
+var publicPath = path.resolve(__dirname, 'public');
+app.use(express.static(publicPath));
 
 app.use(function(request, response) {
   response.writeHead(200, { "Content-Type": "text/plain" });
-  response.end("Hello, world!");
+  response.end("Looks like you didn't find a static file.");
 })
 
 http.createServer(app).listen(3000);
