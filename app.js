50906f8d8fae2c48ded18ab7c872e1e9a4c9e047
Lionel T <elrumordelaluz@hotmail.com>
2016-04-23 23:11:43 +0200

    API: versioning

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
 var express = require('express');
 
+var apiVersion1 = require('./api1');
+var apiVersion2 = require('./api2');
+
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send('You just sent a GET request.')
-});
-
-app.post('/', function(req, res) {
-  res.send('You just sent a POST request.')
-});
-
-app.put('/', function(req, res) {
-  res.send('You just sent a PUT request.')
-});
-
-app.delete('/', function(req, res) {
-  res.send('You just sent a DELETE request.')
-});
+app.use('/v1', apiVersion1);
+app.use('/v2', apiVersion2);
 
 app.listen(3000, function() {
   console.log('App started on Port 3000.')
