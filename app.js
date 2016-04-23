208732074ca6c7a859245974a0dec31c79d1c89d
Lionel T <elrumordelaluz@hotmail.com>
2016-04-23 17:36:33 +0200

    Middleware: custom logger and staticfile

--- app.js
+++ app.js
@@ -1,47 +1,36 @@
-var http = require('http');
-var path = require('path');
 var express = require('express');
-var logger = require('morgan')
-var bodyParser = require('body-parser');
+var path = require('path');
+var fs = require('fs');
 
 var app = express();
 
-app.set('views', path.resolve(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
-var entries = [];
-app.locals.entries = entries;
-
-app.use(logger('dev'));
-
-app.use(bodyParser.urlencoded({ extended: false}));
-
-app.get('/', function(req, res) {
-  res.render('index');
+app.use(function(req, res, next) {
+  console.log("Request IP: " + req.url);
+  console.log("Request date: " + new Date());
+  next();
 });
 
-app.get('/new-entry', function(req, res) {
-  res.render('new-entry');
+app.use(function(req, res, next) {
+  var filePath = path.join(__dirname, "static", req.url);
+  fs.stat(filePath, function(err, fileInfo) {
+    if (err) {
+      next();
+      return;
+}
+    if (fileInfo.isFile()) {
+      res.sendFile(filePath);
+    } else {
+      next();
+} });
 });
 
-app.post('/new-entry', function(req, res) {
-  if (!req.body.title || !req.body.body) {
-    res.status(400).send('Entries must have a title and a body');
-    return;
-  }
-  entries.push({
-    title: req.body.title,
-    content: req.body.body,
-    published: new Date()
-  });
-
-  res.redirect('/');
-});
 
 app.use(function(req, res) {
-  res.status(404).render('404');
-});
+  res.status(404);
+  res.send('File not found!');
+})
 
-http.createServer(app).listen(3000, function() {
-  console.log('Gestbook app started on port 3000');
-});
+app.listen(3000, function() {
+  console.log('App started on Port: 3000.');
+})
