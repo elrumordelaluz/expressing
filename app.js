a0011fc059a98fb345f0ca706f0d45d121696b99
Lionel T <elrumordelaluz@hotmail.com>
2016-04-23 17:38:07 +0200

    Middleware: logger using Morgan and express.static

--- app.js
+++ app.js
@@ -1,29 +1,13 @@
 var express = require('express');
+var morgan = require('morgan');
 var path = require('path');
-var fs = require('fs');
 
 var app = express();
+var morganMiddleware = morgan('short');
+var staticPath = path.join(__dirname, 'static');
 
-
-app.use(function(req, res, next) {
-  console.log("Request IP: " + req.url);
-  console.log("Request date: " + new Date());
-  next();
-});
-
-app.use(function(req, res, next) {
-  var filePath = path.join(__dirname, "static", req.url);
-  fs.stat(filePath, function(err, fileInfo) {
-    if (err) {
-      next();
-      return;
-}
-    if (fileInfo.isFile()) {
-      res.sendFile(filePath);
-    } else {
-      next();
-} });
-});
+app.use(morganMiddleware);
+app.use(express.static(staticPath));
 
 
 app.use(function(req, res) {
