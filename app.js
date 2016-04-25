9b5405bd28661f991e3f440124cc9de55f8477c9
Lionel T <elrumordelaluz@hotmail.com>
2016-04-25 18:36:17 +0200

    MongoDB: User authetication

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var path = require('path');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var flash = require('connect-flash');
+var passport = require('passport');
 
-var apiVersion1 = require('./api1');
-var apiVersion2 = require('./api2');
+var routes = require('./routes');
+var setUpPassport = require('./setuppassport.js');
 
 var app = express();
 
-app.use('/v1', apiVersion1);
-app.use('/v2', apiVersion2);
+mongoose.connect('mongodb://localhost:27017/test');
 
-app.listen(3000, function() {
-  console.log('App started on Port 3000.')
+setUpPassport();
+
+app.set('port', process.env.PORT || 3000);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
+app.use(session({
+  secret: "TKRv0IJs=HYqrvagQ#&!F!%V]Ww/4KiVs$s,<<MX",
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use(routes);
+
+app.listen(app.get('port'), function() {
+  console.log('Server started on port ' + app.get('port'));
 });
