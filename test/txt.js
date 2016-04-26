9349ca1a0f448535182a9ca1ff909cae7e7f3b37
Lionel T <elrumordelaluz@hotmail.com>
2016-04-26 11:20:28 +0200

    Reducing repetition with beforeEach

--- test/txt.js
+++ test/txt.js
@@ -2,21 +2,23 @@ var supertest = require('supertest');
 var app = require('../app');
 
 describe('plain text response', function() {
-  it('returns a plain text response', function(done) {
-    supertest(app)
+  var request;
+  beforeEach(function() {
+    request = supertest(app)
       .get('/')
       .set('User-Agent', 'my cool browser')
-      .set('Accept', 'text/plain')
+      .set('Accept', 'text/plain');
+  });
+
+  it('returns a plain text response', function(done) {
+    request
       .expect('Content-Type', /text\/plain/)
       .expect(200)
       .end(done);
   });
 
   it('return your User Agent', function(done) {
-    supertest(app)
-      .get('/')
-      .set('User-Agent', 'my cool browser')
-      .set('Accept', 'text/plain')
+    request
       .expect(function(res) {
         if (res.text !== 'my cool browser') {
           throw new Error('Response does not contain User Agent');
