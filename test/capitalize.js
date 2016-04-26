4c2d04d2cdcf39b82e2b326d8bf4cea11d47e97a
Lionel T <elrumordelaluz@hotmail.com>
2016-04-26 10:42:45 +0200

    Add more tests

--- test/capitalize.js
+++ test/capitalize.js
@@ -8,4 +8,34 @@ describe('capitalize', function() {
     expect(capitalize('express')).to.equal('Express');
     expect(capitalize('cats')).to.equal('Cats');
   });
+
+  it('makes the rest of the string lowercase', function() {
+    expect(capitalize('javaScript')).to.equal('Javascript');
+  });
+
+  it('leaves empty strings alone', function() {
+    expect(capitalize('')).to.equal('');
+  });
+
+  it('leaves strings with no words alone', function() {
+    expect(capitalize('   ')).to.equal('   ');
+    expect(capitalize('123')).to.equal('123');
+  });
+
+  it('capitalize multiple-word strings', function() {
+    expect(capitalize('what is Express?')).to.equal('What is express?');
+    expect(capitalize('i love lamp')).to.equal('I love lamp');
+  });
+
+  it('leaves already-capitalized words alone', function() {
+    expect(capitalize('Express')).to.equal('Express');
+    expect(capitalize('Lorem')).to.equal('Lorem');
+    expect(capitalize('Ipsum')).to.equal('Ipsum');
+  });
+
+  it('capitalize String objects without changing their values', function() {
+    var str = new String('who is JavaScript?');
+    expect(capitalize(str)).to.equal('Who is javascript?');
+    expect(str.valueOf()).to.equal('who is JavaScript?');
+  });
 });
